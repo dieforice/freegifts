e3159e26a89ce5fe784821205527e7d05017a237
NGUYENLAMHAI89\Asus <nguyenlamhai89@gmail.com>
2017-10-23 21:26:50 +0700

    Commited

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "jroweror3PƯƠ]ơ4oo32porwe342e3&^&^&#^@)(@(#or4343r"
 mlab.connect()
 
-@app.route('/')
+@app.route('/',  methods = ["GET", "POST"])
 def index():
     return render_template('index.html', questions=Question.objects())
 
 @app.route('/admin')
 def admin():
     if "admin" not in session:
-        abort(403)
+        return abort(403)
     else:
         return render_template('admin.html', questions=Question.objects())
 
@@ -28,23 +28,12 @@ def login():
         username = form["username"]
         password = form["password"]
 
-        user = User.objects(username=username).first()
-        if user is None:
-            flash("No such user")
-            return render_template("login.html")
-        else:
-            if user.password != password:
-                flash("Wrong Password")
-                return render_template('login.html')
-            else:
-                session['admin'] = True
-                return redirect('admin')
-        if username == "admin" and password == "admin":
-            session['admin'] = True
-            return redirect('/admin')
-        else:
-            flash("Wrong username or password")
-            return render_template("login.html")
+
+    if username == "admin" and password == "admin":
+        session['admin'] = True
+        return redirect('/admin')
+    else:
+        return abort(403)
 
 
 if __name__ == '__main__':

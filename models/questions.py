6ffcba34a91a46eb3cfc3daea8bb2eb14abd541b
dieforice <dieforice@gmail.com>
2017-10-27 15:33:35 +0700

    no message

--- models/questions.py
+++ models/questions.py
@@ -2,6 +2,7 @@ from mongoengine import *
 from faker import Faker
 import mlab
 mlab.connect()
+
 class Question(Document): #Collection
     question = StringField()
     answerA = StringField()
@@ -9,10 +10,14 @@ class Question(Document): #Collection
     answerC = StringField()
     answerD = StringField()
     correct_answer = StringField()
+class Stress(Document): #Collection
+    # = StringField()
+    answerA = StringField()
+    answerB = StringField()
+    answerC = StringField()
+    answerD = StringField()
+    correct_answer = StringField()
 
-<<<<<<< HEAD
-#task = Question(question = "If I _____ an animal, I ______ a cat", answerA="was/will be", answerB = "was/would be", answerC = "were/will be", answerD = "were/would be", correct_answer = "were/would be")
-=======
 #task = Question(question = "Alexander the Great created one of the largest _______ of the ancient world ____ the age of thirty", answerA = "kingdoms/up until", answerB = "empires/at", answerC = "empires/by", answerD = "nations/at", correct_answer = "empires/by")
->>>>>>> b997075ffa28c1a077ff2f862f338368632575f3
-#task.save()
+#task = Stress(answerA = "technical", answerB = "government", answerC = "parallel", answerD = "understand", correct_answer= "understand")
+task.save()

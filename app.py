30bf038347faf94fe51bf7ec6b29c50468f91c6c
dieforice <dieforice@gmail.com>
2017-10-27 16:31:47 +0700

    no message

--- app.py
+++ app.py
@@ -16,9 +16,24 @@ def index():
         b = random.randint(11,21)
         c = random.randint(21,31)
         d = random.randint(31,41)
-        return render_template('index.html', questions=Question.objects()[a], questions2=Question.objects()[b], questions3=Question.objects()[c], questions4=Question.objects()[d])
+        return render_template('index.html', questions=Question.objects()[a], questions2=Question.objects()[b], questions3=Question.objects()[c], questions4=Question.objects()[d], stress=Stress.objects()[a])
     elif request.method == "POST":
         form = request.form
+        answer1 = form["answer1"]
+        answer2 = form["answer2"]
+        answer3 = form["answer3"]
+        answer4 = form["answer4"]
+        answer5 = form["answer5"]
+        if answer1 == Question.objects(correct_answer)[a]:
+            return render_template('login.html')
+        if answer2 == Question.objects(correct_answer)[b]:
+            return render_template('login.html')
+        if answer3 == Question.objects(correct_answer)[c]:
+            return render_template('login.html')
+        if answer4 == Question.objects(correct_answer)[d]:
+            return render_template('login.html')
+        if answer5 == Stress.objects(correct_answer)[a]:
+            return render_template('login.html')
 @app.route('/admin')
 def admin():
     if "admin" not in session:
